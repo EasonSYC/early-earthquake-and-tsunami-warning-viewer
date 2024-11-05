372f61e541dfb753e164269dd7ab4ffb45d0849d
Eason Shao <eason.syc@icloud.com>
2024-11-05 16:00:47 +0000

    add some comments.

--- EasonEetwViewer/EasonEetwViewer.Dto/ResponseEnums/Status.cs
+++ EasonEetwViewer/EasonEetwViewer.Dto/ResponseEnums/Status.cs
@@ -2,11 +2,20 @@ using System.Text.Json.Serialization;
 
 namespace EasonEetwViewer.Dto.ResponseEnums;
 
+/// <summary>
+/// Represents the status of the API response.
+/// </summary>
 [JsonConverter(typeof(JsonStringEnumConverter<Status>))]
 public enum Status
 {
+    /// <summary>
+    /// The <c>ok</c> value, representing a success.
+    /// </summary>
     [JsonStringEnumMemberName("ok")]
     Ok,
+    /// <summary>
+    /// The <c>error</c> value, representing an error.
+    /// </summary>
     [JsonStringEnumMemberName("error")]
     Error
 }
--- EasonEetwViewer/EasonEetwViewer.Dto/ResponseModels/ApiResponse.cs
+++ EasonEetwViewer/EasonEetwViewer.Dto/ResponseModels/ApiResponse.cs
@@ -2,12 +2,25 @@
 
 namespace EasonEetwViewer.Dto.ResponseModels;
 
+/// <summary>
+/// Outlines the model of a HTTP response in JSON format from an API Call.
+/// Abstract and cannot be instantiated.
+/// </summary>
 public abstract record ApiResponse
 {
+    /// <summary>
+    /// The <c>responseId</c> property.
+    /// </summary>
     [JsonPropertyName("responseId")]
     public string Id { get; init; } = string.Empty;
+    /// <summary>
+    /// The <c>responseTime</c> property.
+    /// </summary>
     [JsonPropertyName("responseTime")]
     public DateTime Time { get; init; }
+    /// <summary>
+    /// The <c>status</c> property.
+    /// </summary>
     [JsonPropertyName("status")]
     public ResponseEnums.Status Status { get; init; }
 }
