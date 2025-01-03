a6d283cf90305676ba91f50e3a404c76effad543
Eason Shao <eason.syc@icloud.com>
2025-01-03 18:04:09 +0800

    separate files and classes.

---
+++ EasonEetwViewer/EasonEetwViewer/ViewModels/ListItemTemplate.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace EasonEetwViewer.ViewModels;
+internal class ListItemTemplate
+{
+    internal ListItemTemplate(Type type, string iconKey)
+    {
+        ModelType = type;
+        Label = type.Name.Replace("PageViewModel", "");
+
+        object instance = Activator.CreateInstance(ModelType)!;
+        Model = (ViewModelBase)instance;
+
+        _ = Application.Current!.TryFindResource(iconKey, out object? res);
+        ListItemIcon = (StreamGeometry)res!;
+    }
+
+    internal string Label { get; init; }
+    internal Type ModelType { get; init; }
+    internal ViewModelBase Model { get; init; }
+    internal StreamGeometry ListItemIcon { get; init; }
+}
--- EasonEetwViewer/EasonEetwViewer/ViewModels/MainWindowViewModel.cs
+++ EasonEetwViewer/EasonEetwViewer/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Media;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -31,24 +28,4 @@ internal partial class MainWindowViewModel : ViewModelBase
 
     [RelayCommand]
     private void TriggerPane() => IsPaneOpen ^= true;
-}
-
-internal class ListItemTemplate
-{
-    internal ListItemTemplate(Type type, string iconKey)
-    {
-        ModelType = type;
-        Label = type.Name.Replace("PageViewModel", "");
-
-        object instance = Activator.CreateInstance(ModelType)!;
-        Model = (ViewModelBase)instance;
-
-        _ = Application.Current!.TryFindResource(iconKey, out object? res);
-        ListItemIcon = (StreamGeometry)res!;
-    }
-
-    internal string Label { get; init; }
-    internal Type ModelType { get; init; }
-    internal ViewModelBase Model { get; init; }
-    internal StreamGeometry ListItemIcon { get; init; }
 }
