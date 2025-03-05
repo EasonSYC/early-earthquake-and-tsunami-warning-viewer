5b22d9844494b5b83dcd0fdf1df9fc06a8f3e383
Eason Shao <eason.syc@icloud.com>
2025-03-05 01:14:49 +0000

    add two extensions.

---
+++ EasonEetwViewer/EasonEetwViewer/Extensions/InformationalTextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasonEetwViewer.Dmdata.Telegram.Dtos.Schema;
+
+namespace EasonEetwViewer.Extensions;
+
+internal static class InformationalTextExtensions
+{
+    private static StringBuilder AppendLineIfVisible(this StringBuilder sb, string? value)
+        => !string.IsNullOrWhiteSpace(value)
+            ? sb.AppendLine(value)
+            : sb;
+    public static string ToInformationalString(this EarthquakeInformationSchema earthquake)
+        => new StringBuilder()
+            .AppendLineIfVisible(earthquake.Headline)
+            .AppendLineIfVisible(earthquake.Body.Text)
+            .AppendLineIfVisible(earthquake.Body.Comments?.FreeText)
+            .AppendLineIfVisible(earthquake.Body.Comments?.Forecast?.Text)
+            .AppendLineIfVisible(earthquake.Body.Comments?.Var?.Text)
+            .ToString();
+}
---
+++ EasonEetwViewer/EasonEetwViewer/Extensions/StringColourExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Mapsui.Styles;
+
+namespace EasonEetwViewer.Extensions
+{
+    internal static class StringColourExtensions
+    {
+        public static Color ToColour(this string colourString)
+            => Color.FromString(colourString);
+    }
+}
