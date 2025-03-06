dca6abddfee4ea06688eb3f368e230bb5302060a
Eason Shao <eason.syc@icloud.com>
2025-03-06 23:13:36 +0000

    i am stupid.

--- EasonEetwViewer/EasonEetwViewer/Logging/ViewModels/ViewModelBases/ViewModelBaseLogs.cs
+++ EasonEetwViewer/EasonEetwViewer/Logging/ViewModels/ViewModelBases/ViewModelBaseLogs.cs
@@ -14,7 +14,7 @@ internal static partial class MainWindowViewModelLogs
         EventId = 0,
         EventName = nameof(Instantiated),
         Level = LogLevel.Information,
-        Message = "Instantiated..")]
+        Message = "Instantiated.")]
     public static partial void Instantiated(
         this ILogger<ViewModelBase> logger);
 }
